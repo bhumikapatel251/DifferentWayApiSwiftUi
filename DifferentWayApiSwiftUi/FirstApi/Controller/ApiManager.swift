7ba0c8adc85e5c32bc412d15e19ce26dfbebe758
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-10 16:34:26 +0530

    set url for json and print data

--- DifferentWayApiSwiftUi/FirstApi/Controller/ApiManager.swift
+++ DifferentWayApiSwiftUi/FirstApi/Controller/ApiManager.swift
@@ -13,3 +13,28 @@ class ApiManager: ObservableObject {
     @Published var items = [Model]()
     
     
+    func fetchData() {
+        let api = "https://jsonplaceholder.typicode.com/todos"
+        guard let url = URL(string: api) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url){
+            (data, response, error) in
+            do {
+                if let data1 = data {
+                    let result = try JSONDecoder().decode([Model].self, from: data1)
+                    DispatchQueue.main.async {
+                        self.items = result
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch (let error) {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+  
+   
+}
