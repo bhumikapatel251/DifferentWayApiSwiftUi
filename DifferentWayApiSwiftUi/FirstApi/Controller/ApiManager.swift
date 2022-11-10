cd3407193930d89694aa0fa56fe60ef4b39ed457
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-10 16:33:39 +0530

    create api manager for fetch api connect to view

---
+++ DifferentWayApiSwiftUi/FirstApi/Controller/ApiManager.swift
@@ -0,0 +1,15 @@
+//
+//  ApiManager.swift
+//  DifferentWayApiSwiftUi
+//
+//  Created by Bhumika Patel on 10/11/22.
+//
+
+import Foundation
+import Foundation
+import SwiftUI
+class ApiManager: ObservableObject {
+    
+    @Published var items = [Model]()
+    
+    
