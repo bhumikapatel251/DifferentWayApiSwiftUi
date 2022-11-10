f72211d6a113f9ce06b546ce31b83bdc15b8ef8b
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-10 16:52:30 +0530

    create quoteApiView

---
+++ DifferentWayApiSwiftUi/QuoteAsyncApi/QuoteApiView.swift
@@ -0,0 +1,20 @@
+//
+//  QuoteApiView.swift
+//  DifferentWayApiSwiftUi
+//
+//  Created by Bhumika Patel on 10/11/22.
+//
+
+import SwiftUI
+
+struct QuoteApiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuoteApiView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuoteApiView()
+    }
+}
