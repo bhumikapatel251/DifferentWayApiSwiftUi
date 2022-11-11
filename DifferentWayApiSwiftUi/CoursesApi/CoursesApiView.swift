7038371a401d9ef59a906926533c67ad70a33009
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-11 15:13:00 +0530

    coursesapiview create text content type , url, request, and response

--- DifferentWayApiSwiftUi/CoursesApi/CoursesApiView.swift
+++ DifferentWayApiSwiftUi/CoursesApi/CoursesApiView.swift
@@ -10,26 +10,33 @@ import SwiftUI
 struct CoursesApiView: View {
     @StateObject var viewModel = ViewModel()
     var body: some View {
-        NavigationView{
-            List{
-                ForEach(viewModel.course, id: \.self ){ course in
-                    HStack{
-                        URLImage(urlString: course.image)
+        VStack(alignment:.leading){
+            Text("Content-Type: text/html; charset=UTF-8")
+            Text("URL: https://iosacademy.io/api/v1/courses/index.php")
+            Text("Req:")
+            Text("Res:")
+            NavigationView{
+                List{
+                    ForEach(viewModel.course, id: \.self ){ course in
+                        HStack{
+                            URLImage(urlString: course.image)
                             
                             
-                        Text(course.name)
-                            .bold()
-                        
-                        
+                            Text(course.name)
+                                .bold()
+                            
+                            
+                        }
+                        .padding(3)
                     }
-                    .padding(3)
+                }
+                .navigationTitle("CoursesList")
+                .onAppear{
+                    viewModel.fetch()
                 }
             }
-            .navigationTitle("CoursesList")
-            .onAppear{
-                viewModel.fetch()
-            }
         }
+        .padding(10)
     }
 }
 
