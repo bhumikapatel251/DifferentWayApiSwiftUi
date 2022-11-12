8727ab4fd8cb3db4dc7fe294f09c74e6e2871767
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-12 17:06:38 +0530

    environment variable set for used state object

--- DifferentWayApiSwiftUi/RequireUser/RequireUserApiView.swift
+++ DifferentWayApiSwiftUi/RequireUser/RequireUserApiView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct RequireUserApiView: View {
+    @StateObject var createDataVM  = DataViewModel()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
